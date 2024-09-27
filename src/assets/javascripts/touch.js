faca632778112cdb3ed93be4fdfc700877af6be3
Kyle Borash <kyle@borash.com>
2024-09-26 21:37:27 -0800

    Add logic to ignore vertical and diagonal swipes.

--- src/assets/javascripts/touch.js
+++ src/assets/javascripts/touch.js
@@ -2,16 +2,20 @@
 let screenWidth = window.innerWidth;
 let touchStartX = 0
 let touchEndX = 0
+let touchStartY = 0
+let touchEndY = 0
 
 // The item pane is identified by the id `col-item`. We only want to handle swipes within that element.
 let item_element = document.getElementById('col-item')
 
 item_element.addEventListener('touchstart', function (event) {
     touchStartX = event.changedTouches[0].screenX;
+    touchStartY = event.changedTouches[0].screenY;
 })
 
 item_element.addEventListener('touchend', function (event) {
     touchEndX = event.changedTouches[0].screenX;
+    touchEndY = event.changedTouches[0].screenY;
     handleSwipe();
 })
 
@@ -23,11 +27,16 @@ window.addEventListener('resize', function (event) {
 // Threshold of 100 should avoid accidental changes when scrolling up and down.
 function handleSwipe() {
     let threshold = Math.floor(screenWidth * .2);
-    if (touchEndX < touchStartX - threshold) {
-        // Swiped left
-        helperFunctions.navigateToItem(+1);
-    } else if (touchEndX > touchStartX + threshold) {
-        // Swiped right
-        helperFunctions.navigateToItem(-1);
+    let diffY = Math.abs(touchEndY - touchStartY);
+    let diffX = Math.abs(touchEndX - touchStartX);
+
+    if (diffX > diffY) {
+        if (touchEndX < touchStartX - threshold) {
+            // Swiped left
+            helperFunctions.navigateToItem(+1);
+        } else if (touchEndX > touchStartX + threshold) {
+            // Swiped right
+            helperFunctions.navigateToItem(-1);
+        }
     }
 }
