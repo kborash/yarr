97372d9cfd9a824657d8ba638c7812a303018e90
Kyle Borash <kyle@borash.com>
2024-10-27 10:41:37 -0800

    Automatically extend sessions that will expire in the next 48 hours.

--- src/server/auth/middleware.go
+++ src/server/auth/middleware.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/nkanaev/yarr/src/assets"
 	"github.com/nkanaev/yarr/src/server/router"
@@ -21,6 +22,23 @@ func unsafeMethod(method string) bool {
 	return method == "POST" || method == "PUT" || method == "DELETE"
 }
 
+func extendSessionIfNeeded(rw http.ResponseWriter, req *http.Request) {
+	cookie, err := req.Cookie("auth")
+	if err != nil {
+		return
+	}
+
+	// Add a week to a session that will expire in the next 48 hours.
+	if time.Until(cookie.Expires) < 48*time.Hour {
+		http.SetCookie(rw, &http.Cookie{
+			Name:    "auth",
+			Value:   cookie.Value,
+			Expires: time.Now().Add(7 * 24 * time.Hour),
+			Path:    cookie.Path,
+		})
+	}
+}
+
 func (m *Middleware) Handler(c *router.Context) {
 	for _, path := range m.Public {
 		if strings.HasPrefix(c.Req.URL.Path, m.BasePath+path) {
@@ -28,7 +46,9 @@ func (m *Middleware) Handler(c *router.Context) {
 			return
 		}
 	}
+
 	if IsAuthenticated(c.Req, m.Username, m.Password) {
+		extendSessionIfNeeded(c.Out, c.Req)
 		c.Next()
 		return
 	}
@@ -51,12 +71,12 @@ func (m *Middleware) Handler(c *router.Context) {
 			c.HTML(http.StatusOK, assets.Template("login.html"), map[string]interface{}{
 				"username": username,
 				"error":    "Invalid username/password",
-		        "settings": m.DB.GetSettings(),
+				"settings": m.DB.GetSettings(),
 			})
 			return
 		}
 	}
 	c.HTML(http.StatusOK, assets.Template("login.html"), map[string]interface{}{
-        "settings": m.DB.GetSettings(),
-    })
+		"settings": m.DB.GetSettings(),
+	})
 }
